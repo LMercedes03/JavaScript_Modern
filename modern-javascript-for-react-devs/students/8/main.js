04ed14d1e7ed808ecbc9e216b9070830a58a2564
Leonard Mercedes <100613411+LMercedes03@users.noreply.github.com>
2023-05-09 16:56:57 -0400

    Complete ex1

--- modern-javascript-for-react-devs/students/8/main.js
+++ modern-javascript-for-react-devs/students/8/main.js
@@ -3,31 +3,42 @@
 // Date: 5/24/2022
 // 
 
-const ex1 = () => {
-    const array = [1, '2', 3, 'test', 1.2];
-    console.log(countNumbers(array));
-}
-
-const ex2 = () => {
-    // TODO...
-}
-
-const ex3 = () => {
-    // TODO...
-}
-
-//
-// Your functions here...
-//
-
+//Ex 1
 const countNumbers = (array) => {
-    let retval = 0;
-    // TODO...
-    return retval;
-}
+    let count = 0;
 
-const main = async () => {
-    ex1();
-}
+    for (let i = 0; i < array.length; i++){
+        if (typeof array[i] == 'number'){ 
+        count++; 
+        }
+    }
 
-main();
+    return count;
+};
+
+const array = [1, '2', 3, 'test', 1.2];
+console.log(countNumbers(array));
+
+// const ex2 = () => {
+//     // TODO...
+// }
+
+// const ex3 = () => {
+//     // TODO...
+// }
+
+// //
+// // Your functions here...
+// //
+
+// const countNumbers = (array) => {
+//     let retval = 0;
+//     // TODO...
+//     return retval;
+// }
+
+// const main = async () => {
+//     ex1();
+// }
+
+// main();

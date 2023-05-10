9c6dc9da45ef871f458bce92f7d456570c12c3d5
Leonard Mercedes <100613411+LMercedes03@users.noreply.github.com>
2023-05-10 11:51:51 -0400

    Complete ex4

--- modern-javascript-for-react-devs/students/8/main.js
+++ modern-javascript-for-react-devs/students/8/main.js
@@ -36,29 +36,52 @@ const minNumber = (array) => {
 const array = [12, 55, 2, 22, 11];
 console.log(minNumber(array));
 
-//Ex 3
-const interleave = (array1, array2) => {
-    if (array1.length !== array2.length) {
-      return 'ERROR: Array length mismatch';
-    }
+// //Ex 3
+// const interleave = (array1, array2) => {
+//     if (array1.length !== array2.length) {
+//       return 'ERROR: Array length mismatch';
+//     }
     
-    let result = '';
+//     let result = '';
     
-    for (let i = 0; i < array1.length; i++) {
-      result += array1[i] + array2[i];
-    }
+//     for (let i = 0; i < array1.length; i++) {
+//       result += array1[i] + array2[i];
+//     }
     
-    return result;
-  };
+//     return result;
+//   };
 
-//Use case for ex3
-const array1 = [1, 2, 3, 4, 5];
-const array2 = ['a', 'b', 'c', 'd', 'e'];
-console.log(interleave(array1, array2));
+// //Use case 1 for ex3
+// const array1 = [1, 2, 3, 4, 5];
+// const array2 = ['a', 'b', 'c', 'd', 'e'];
+// console.log(interleave(array1, array2));
 
-// const ex3 = () => {
-//     // TODO...
-// }
+// //Use case 2 for ex3 (testing mismatch error message)
+// const array1 = [1, 2, 3, 4, 5];
+// const array2 = ['a', 'b', 'c', 'd', 'e'];
+// console.log(interleave(array1, array2));
+
+
+//Ex 4
+const palindrome = (string) =>{
+    
+    const length = Math.floor(string.length/2);
+    
+    for (let i = 0; i < length; i++){
+        if (string[i] !== string[string.length - i - 1])
+            return false;
+        }
+
+    return true;      
+    
+    }
+
+
+    console.log(palindrome('radar'));
+    console.log(palindrome('month'));
+
+
+
 
 // //
 // // Your functions here...
